41c40af556bd119e4e2a877a2f07f819ca062833
Gregory Mertens <info@mertens3d.com>
2021-02-08 14:56:50 -0600

    Adding Create-a-contact-and-an-interaction

---
+++ src/Project/Sitecore.Documentation/Const.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Documentation
+{
+public struct Const
+  {
+
+    public struct Certificate
+    {
+      public static string CertificateStore = "StoreName=My;StoreLocation=LocalMachine;FindType=FindByThumbprint;FindValue=";
+      public static string CertificateThumbprint = "42829cff3fa746441d6faaf8e6af216c0063a685";
+    }
+
+    public struct EndPoints
+    {
+      public static string Odata = "https://LearnEXMxconnect.dev.local/odata";
+      public static string Configuration = "https://LearnEXMxconnect.dev.local/configuration";
+    }
+  }
+}
---
+++ src/Project/Sitecore.Documentation/Program.cs
@@ -0,0 +1,92 @@
+﻿using Sitecore.XConnect;
+using Sitecore.XConnect.Client;
+using Sitecore.XConnect.Client.WebApi;
+using Sitecore.XConnect.Collection.Model;
+using Sitecore.XConnect.Schema;
+using Sitecore.Xdb.Common.Web;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sitecore.Documentation
+{
+  internal class Program
+  {
+    private static void Main(string[] args)
+    {
+      MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+      Console.ForegroundColor = ConsoleColor.DarkGreen;
+      Console.WriteLine("");
+      Console.WriteLine("END OF PROGRAM");
+      Console.ReadKey();
+    }
+
+    private static async Task MainAsync(string[] args)
+    {
+      CertificateHttpClientHandlerModifierOptions options = CertificateHttpClientHandlerModifierOptions.Parse(Const.Certificate.CertificateStore + Const.Certificate.CertificateThumbprint);
+
+      var certificateModifier = new CertificateHttpClientHandlerModifier(options);
+
+      List<IHttpClientModifier> clientModifiers = new List<IHttpClientModifier>();
+
+      var timeoutClientModifier = new TimeoutHttpClientModifier(new TimeSpan(0, 0, 20));
+      clientModifiers.Add(timeoutClientModifier);
+
+      var collectionClient = new CollectionWebApiClient(new Uri(Const.EndPoints.Odata), clientModifiers, new[]
+      {
+        certificateModifier
+      });
+
+      var searchClient = new SearchWebApiClient(new Uri(Const.EndPoints.Odata), clientModifiers, new[] {
+        certificateModifier
+      });
+
+      var configurationClient = new ConfigurationWebApiClient(new Uri(Const.EndPoints.Configuration), clientModifiers, new[] { certificateModifier });
+
+      var cfg = new XConnectClientConfiguration(new XdbRuntimeModel(CollectionModel.Model), collectionClient, searchClient, configurationClient);
+
+      try
+      {
+        await cfg.InitializeAsync();
+
+        // Print xConnect if configuration is valid
+        var arr = new[]
+         {
+                        @"            ______                                                       __     ",
+                        @"           /      \                                                     |  \    ",
+                        @" __    __ |  $$$$$$\  ______   _______   _______    ______    _______  _| $$_   ",
+                        @"|  \  /  \| $$   \$$ /      \ |       \ |       \  /      \  /       \|   $$ \  ",
+                        @"\$$\/  $$| $$      |  $$$$$$\| $$$$$$$\| $$$$$$$\|  $$$$$$\|  $$$$$$$ \$$$$$$   ",
+                        @" >$$  $$ | $$   __ | $$  | $$| $$  | $$| $$  | $$| $$    $$| $$        | $$ __  ",
+                        @" /  $$$$\ | $$__/  \| $$__/ $$| $$  | $$| $$  | $$| $$$$$$$$| $$_____   | $$|  \",
+                        @"|  $$ \$$\ \$$    $$ \$$    $$| $$  | $$| $$  | $$ \$$     \ \$$     \   \$$  $$",
+                        @" \$$   \$$  \$$$$$$   \$$$$$$  \$$   \$$ \$$   \$$  \$$$$$$$  \$$$$$$$    \$$$$ "
+                    };
+
+        Console.WindowWidth = 160;
+
+        foreach (string line in arr)
+        {
+          Console.WriteLine(line);
+        }
+      }
+      catch (XdbModelConflictException ce)
+      {
+        Console.WriteLine("Error: " + ce.Message);
+      }
+
+      // Initialize a client using the validate configuration
+
+      using (var client = new XConnectClient(cfg))
+      {
+        try
+        {
+        }
+        catch (XdbExecutionException ex)
+        {
+          // deal with exception
+        }
+      }
+    }
+  }
+}
