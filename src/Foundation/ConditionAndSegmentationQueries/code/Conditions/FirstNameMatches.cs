dc742f7c3b54e017f10ee8fcce63d5307be3c767
Gregory Mertens <info@mertens3d.com>
2021-03-10 09:19:39 -0600

    First Name compare works

---
+++ src/Foundation/ConditionAndSegmentationQueries/code/Conditions/FirstNameMatches.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Framework.Rules;
+using Sitecore.XConnect;
+using Sitecore.XConnect.Collection.Model;
+using Sitecore.XConnect.Segmentation.Predicates;
+using System;
+using System.Linq.Expressions;
+
+namespace LearnEXM.Foundation.ConditionAndSegmentationQueries.Conditions
+{
+  public class FirstNameMatches : ICondition, IContactSearchQueryFactory
+  {
+    public string TargetName { get; set; }
+
+    public StringOperationType Comparison { get; set; }
+
+    public bool Evaluate(IRuleExecutionContext context)
+    {
+      var contact = context.Fact<Contact>();
+
+      return Comparison.Evaluate(contact.GetFacet<PersonalInformation>(PersonalInformation.DefaultFacetKey)?.FirstName, TargetName);
+    }
+
+    // Evaluate contact in a search context
+    // Use InteractionsCache()
+    public Expression<Func<Contact, bool>> CreateContactSearchQuery(IContactSearchQueryContext context)
+    {
+      return contact => Comparison.Evaluate(contact.GetFacet<PersonalInformation>(PersonalInformation.DefaultFacetKey).FirstName, TargetName);
+    }
+  }
+}
